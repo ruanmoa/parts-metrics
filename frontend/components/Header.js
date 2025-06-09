8fd2f2f3948a66a3c54c13f44fdf622eb016f96d
Ruan Andrade <ruan.moa@gmail.com>
2025-06-09 20:58:01 -0300

    Add header with logo area and login button

---
+++ frontend/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="logo">LOGO</div>
+      <button className="login-button">ENTRAR</button>
+    </header>
+  );
+}
